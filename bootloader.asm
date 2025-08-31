2cf5b99bc7d158225a517ed96530eaa015ec9a3f
Kavoshnik <hikkan008@gmail.com>
2025-08-31 17:29:32 +0700

    test kernel

--- bootloader.asm
+++ bootloader.asm
@@ -2,7 +2,6 @@ BITS 16
 ORG 0x7C00
 
 start:
-    ; Установим сегментные регистры
     xor ax, ax
     mov ds, ax
     mov es, ax
@@ -12,35 +11,32 @@ start:
     ; Загрузка ядра ДО перехода в защищённый режим
     call load_kernel
 
-    ; Включим A20 линию
+    ; Включение A20
     in al, 0x92
     or al, 2
     out 0x92, al
 
-    ; Загрузим GDT
+    ; Загрузка GDT
     lgdt [gdt_descriptor]
 
-    ; Переход в Protected Mode
+    ; Переход в защищённый режим
     mov eax, cr0
     or eax, 1
     mov cr0, eax
 
-    ; Длинный переход в 32-битный код
     jmp 0x08:protected_mode
 
-; ========================
-; GDT (перемещено до использования)
-; ========================
+; GDT должна быть объявлена до её использования
 gdt_start:
-    dq 0x0 ; NULL дескриптор
+    dq 0x0
 
 gdt_code:
-    dw 0xFFFF    ; Лимит
-    dw 0x0000    ; База
-    db 0x00      ; База
-    db 10011010b ; Флаги доступа
-    db 11001111b ; Флаги + лимит
-    db 0x00      ; База
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10011010b
+    db 11001111b
+    db 0x00
 
 gdt_data:
     dw 0xFFFF
@@ -56,21 +52,19 @@ gdt_descriptor:
     dw gdt_end - gdt_start - 1
     dd gdt_start
 
-; ========================
-; Загрузка ядра
-; ========================
+; Загрузка ядра через INT 0x13
 load_kernel:
-    mov ah, 0x02    ; Чтение секторов
-    mov al, 20      ; Увеличено количество секторов
-    mov ch, 0       ; Цилиндр
-    mov cl, 2       ; Сектор (начинаем с 2)
-    mov dh, 0       ; Головка
-    mov dl, 0x00    ; Флоппи диск
-    mov bx, 0x1000  ; ES:BX = 0x1000:0x0000
+    mov ah, 0x02
+    mov al, 50      ; Увеличено до 50 секторов
+    mov ch, 0
+    mov cl, 2
+    mov dh, 0
+    mov dl, 0x00
+    mov bx, 0x1000  ; ES:BX = 0x1000:0x0000 -> 0x10000
     mov es, bx
     xor bx, bx
     int 0x13
-    jc disk_error   ; Если ошибка
+    jc disk_error
     ret
 
 disk_error:
@@ -89,26 +83,25 @@ print_string:
 .done:
     ret
 
-error_msg db "Disk read error!", 0
+error_msg db "Disk error!", 0
 
-; ========================
-; 32-битный режим
-; ========================
 BITS 32
-
 protected_mode:
-    ; Установим сегментные регистры
     mov ax, 0x10
     mov ds, ax
     mov es, ax
-    mov fs, ax
-    mov gs, ax
     mov ss, ax
     mov esp, 0x90000
 
-    ; Передадим управление ядру
+    ; Копируем ядро из 0x10000 в 0x100000
+    cld
+    mov esi, 0x10000
+    mov edi, 0x100000
+    mov ecx, 50 * 512  ; 50 секторов по 512 байт
+    rep movsb
+
+    ; Переход на ядро
     jmp 0x100000
 
-; Заполнение до 510 байт
-times 510 - ($ - $$) db 0
+times 510 - ($-$$) db 0
 dw 0xAA55
