699b423856aba1efe10a2e7cc620e7804b1d8591
Kavoshnik <hikkan008@gmail.com>
2025-08-31 17:23:52 +0700

    general fix(now)

--- bootloader.asm
+++ bootloader.asm
@@ -2,6 +2,7 @@ BITS 16
 ORG 0x7C00
 
 start:
+    ; Установим сегментные регистры
     xor ax, ax
     mov ds, ax
     mov es, ax
@@ -11,42 +12,73 @@ start:
     ; Загрузка ядра ДО перехода в защищённый режим
     call load_kernel
 
-    ; Включение A20
+    ; Включим A20 линию
     in al, 0x92
     or al, 2
     out 0x92, al
 
-    ; Загрузка GDT
+    ; Загрузим GDT
     lgdt [gdt_descriptor]
 
-    ; Переход в защищённый режим
+    ; Переход в Protected Mode
     mov eax, cr0
     or eax, 1
     mov cr0, eax
 
+    ; Длинный переход в 32-битный код
     jmp 0x08:protected_mode
 
-; Загрузка ядра через INT 0x13 (реальный режим)
+; ========================
+; GDT (перемещено до использования)
+; ========================
+gdt_start:
+    dq 0x0 ; NULL дескриптор
+
+gdt_code:
+    dw 0xFFFF    ; Лимит
+    dw 0x0000    ; База
+    db 0x00      ; База
+    db 10011010b ; Флаги доступа
+    db 11001111b ; Флаги + лимит
+    db 0x00      ; База
+
+gdt_data:
+    dw 0xFFFF
+    dw 0x0000
+    db 0x00
+    db 10010010b
+    db 11001111b
+    db 0x00
+
+gdt_end:
+
+gdt_descriptor:
+    dw gdt_end - gdt_start - 1
+    dd gdt_start
+
+; ========================
+; Загрузка ядра
+; ========================
 load_kernel:
-    mov ah, 0x02
-    mov al, 20       ; Увеличьте число секторов при росте ядра
-    mov ch, 0
-    mov cl, 2        ; Начинаем с сектора №2
-    mov dh, 0
-    mov dl, 0x00
-    mov bx, 0x1000   ; ES:BX = 0x1000:0x0000 -> 0x10000
+    mov ah, 0x02    ; Чтение секторов
+    mov al, 20      ; Увеличено количество секторов
+    mov ch, 0       ; Цилиндр
+    mov cl, 2       ; Сектор (начинаем с 2)
+    mov dh, 0       ; Головка
+    mov dl, 0x00    ; Флоппи диск
+    mov bx, 0x1000  ; ES:BX = 0x1000:0x0000
     mov es, bx
     xor bx, bx
     int 0x13
-    jc .error        ; Проверка ошибок
+    jc disk_error   ; Если ошибка
     ret
-.error:
+
+disk_error:
     mov si, error_msg
-    call print_real
+    call print_string
     hlt
 
-; Вывод сообщения в реальном режиме
-print_real:
+print_string:
     mov ah, 0x0E
 .loop:
     lodsb
@@ -57,21 +89,26 @@ print_real:
 .done:
     ret
 
-error_msg db "Disk error!", 0
+error_msg db "Disk read error!", 0
 
-; GDT и данные...
-; ... (остальной код без изменений)
-
+; ========================
+; 32-битный режим
+; ========================
 BITS 32
+
 protected_mode:
+    ; Установим сегментные регистры
     mov ax, 0x10
     mov ds, ax
     mov es, ax
+    mov fs, ax
+    mov gs, ax
     mov ss, ax
     mov esp, 0x90000
 
-    ; Прыжок на ядро
-    jmp 0x100000  ; Теперь ядро загружено правильно
+    ; Передадим управление ядру
+    jmp 0x100000
 
-times 510 - ($-$$) db 0
+; Заполнение до 510 байт
+times 510 - ($ - $$) db 0
 dw 0xAA55
