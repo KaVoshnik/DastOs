44c703ea37cc82c02a85ce2ec6b44035b5d763d6
Kavoshnik <hikkan008@gmail.com>
2025-09-01 18:31:49 +0700

    writestring function

--- kernel.c
+++ kernel.c
@@ -1,11 +1,16 @@
-void kernel_main() {
+// kernel.c
+void terminal_putchar(char c) {
     char* video = (char*)0xB8000;
-    video[0] = 'M';
-    video[1] = 0x07;
-    video[2] = 'y';
-    video[3] = 0x07;
-    video[4] = 'O';
-    video[5] = 0x07;
-    video[6] = 's';
-    video[7] = 0x07;
+    static int pos = 0;
+    video[pos++] = c;
+    video[pos++] = 0x07;
+}
+
+void terminal_writestring(const char* data) {
+    for (int i = 0; data[i] != '\0'; i++)
+        terminal_putchar(data[i]);
+}
+
+void kernel_main() {
+    terminal_writestring("Welcome to MyOS!\n");
 }
